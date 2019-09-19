47243086601c6a93c0f5998620fb3c9ab0281df6
Thomas Janssen <thomas.janssen@rubix.nl>
2019-09-19 20:43:00 +0200

    update

--- src/main/java/nl/terra10/api/TodosResource.java
+++ src/main/java/nl/terra10/api/TodosResource.java
@@ -15,11 +15,15 @@ public class TodosResource {
     TodoService todoService;
 
     @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
     public List<Todo> list() {
         return todoService.list();
     }
 
     @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
     public List<Todo> add(Todo todo) {
         todoService.add(todo);
         return list();
@@ -27,12 +31,16 @@ public class TodosResource {
 
     @DELETE
     @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
     public List<Todo> delete(@PathParam("id") int id) {
         todoService.remove(id);
         return list();
     }
 
     @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
     public List<Todo> delete() {
         todoService.removeAll();
         return list();
